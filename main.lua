2a68f2b99d39d0945289da0cd1a27a9c776285b5
Suraj Wate <suraj.wate@gmail.com>
2020-11-09 10:31:43 +0530

    paddles and ball added

--- main.lua
+++ main.lua
@@ -15,7 +15,7 @@ function love.load()
 
     -- set new font as LOVE2D's active font
     love.graphics.setFont(smallFont)
-    
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -30,12 +30,23 @@ function love.keypressed(key)
 end
 
 function love.draw()
+    -- begin rendering at virtual level
     push:apply('start')
-    love.graphics.printf(
-        "Hello Pong!",
-        0,
-        VIRTUAL_HEIGHT/2 - 6,
-        VIRTUAL_WIDTH,
-        'center')
+
+    -- clear the screen with specific color
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    -- draw welcome text towards the top of screen
+    love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, 'center')
+
+    -- render first paddle (left side)
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    
+    -- render second paddle (right side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- render ball at center
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH /2 - 2, VIRTUAL_HEIGHT /2 - 2, 4, 4)
+
     push:apply('end')
 end
